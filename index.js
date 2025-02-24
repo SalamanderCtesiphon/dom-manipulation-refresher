dfc47a3f24539a63e7e3a051b1723908f1196a3a
SalamanderCtesiphon <swbrookshire@gmail.com>
2025-02-24 06:25:31 -0600

    began following the examples. fixes a typo in the html. add a style rule.

---
+++ index.js
@@ -0,0 +1,8 @@
+const container = document.querySelector("#container");
+
+//const display = container.firstElementChild;
+
+const controls = document.querySelector(".controls");
+
+const display = controls.previousElementSibling;
+console.log(display);
